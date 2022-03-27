c0c9d0090d98a34c2b5f1993a6d549220fa30aa6
Matthew Akuhata <matthewakuhata@gmail.com>
2022-03-27 17:21:39 +1100

    Add favourites endpoints for images
    * Saving as favourite - saveImageAsFavourite()
    * Fetching favourites - getImageById()
    * Deleting favourite image - deleteFavourite()

--- server/src/controllers/images.ts
+++ server/src/controllers/images.ts
@@ -33,7 +33,57 @@ const getImageById = async (
 	return res.status(200).json(data);
 };
 
+const getFavoriteImages = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const path = "favourites?sub_id=me";
+
+	const result: AxiosResponse = await catsHttpService.get(path, {});
+	const data = result.data || [];
+	return res.status(200).json(data);
+};
+
+const saveImageAsFavourite = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const body = {
+		image_id: req.params.id,
+		sub_id: "me",
+	};
+	const result: AxiosResponse = await catsHttpService.post(
+		`favourites`,
+		body,
+		{}
+	);
+
+	const data = result.data || [];
+	return res.status(200).json(data);
+};
+
+const deleteFavourite = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const id = req.params.id;
+
+	const result: AxiosResponse = await catsHttpService.delete(
+		`favourites/${id}`,
+		{}
+	);
+
+	const data = result.data || [];
+	return res.status(200).json(data);
+};
+
 export default {
 	getAllImages,
 	getImageById,
+	getFavoriteImages,
+	saveImageAsFavourite,
+	deleteFavourite,
 };

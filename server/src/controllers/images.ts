ac0ab1d986a3022742fd2c8e1b5251629cd9feae
Matthew Akuhata <matthewakuhata@gmail.com>
2022-03-27 17:13:35 +1100

    Create cat images controller

---
+++ server/src/controllers/images.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { AxiosResponse } from "axios";
+import catsHttpService from "../services/cats.https";
+
+const getAllImages = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const limit = req.query.limit || 1;
+	const breed_id = req.query.breed_id;
+	try {
+		const result: AxiosResponse = await catsHttpService.get(
+			`images/search?limit=${limit}&breed_id=${breed_id}`,
+			{}
+		);
+		const data = result.data || [];
+		return res.status(200).json(data);
+	} catch (err: any) {
+		return res.status(404).json({ message: err.data?.message });
+	}
+};
+
+export default { getAllImages }

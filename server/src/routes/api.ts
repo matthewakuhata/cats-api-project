f14021fa8566855fbab945acab0ead630cf40a24
Matthew Akuhata <matthewakuhata@gmail.com>
2022-03-27 17:53:01 +1100

    Add /images to api

--- server/src/routes/api.ts
+++ server/src/routes/api.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import catRoutes from "./cats/cats.router";
+import imageRoutes from "./images/images.router";
+
 const api = express.Router();
 
 api.use("/cats", catRoutes);
+api.use("/images", imageRoutes);
+
 export default api;
